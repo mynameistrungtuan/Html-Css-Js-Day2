f4be83e33d7b10fc43d1d4818be87181f6bc25e5
mynameistrungtuan <mynameistrungtuan@gmail.com>
2022-02-15 21:44:21 +0700

    change day and night

---
+++ day-night.js
@@ -0,0 +1,9 @@
+$('#switch').click(function () {
+    if ($('#fullpage').hasClass("night")) {
+        $('#fullpage').removeClass("night");
+        $('#switch').removeClass("switched");
+    }else {
+        $('#fullpage').addClass("night");
+        $('#switch').addClass("switched");
+    }
+});
